194b97a8803c371ab01ea0cb7fc317ba3ad13980
kevin_678 <kevin_678@126.com>
2019-06-28 23:45:34 +0800

    feat: update

---
+++ gateway/src/main/java/com/winfred/spring/cloud/app/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.winfred.spring.cloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.winfred.spring.cloud.config"
+        }
+)
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
